8838204283745a34d6ad6a611ce899a15078bc67
M-Abdullah-Sheikh <afsahfarooqui234@gmail.com>
2022-11-25 09:13:08 +0500

    edit krnaw ha aur bhi

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
 
 import express from "express";
+import path from 'path';
 const app = express()
 const port = process.env.PORT||3000
 
 app.get('/', (req, res) => {
   res.send('Hello sylani!')
 })
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
